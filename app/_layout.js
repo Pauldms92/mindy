736da1b5bee83b7d43181f75f768506d9dc98867
Axelmi1 <146069083+Axelmi1@users.noreply.github.com>
2025-11-19 13:59:33 +0100

    Revert "Fix: Protection contre les appels multiples lors de la reconnexion"

--- app/_layout.js
+++ app/_layout.js
@@ -3,13 +3,6 @@ import { Stack } from 'expo-router';
 import { View, StatusBar } from 'react-native';
 import { OnboardingProvider } from '../lib/OnboardingContext';
 
-export default function RootLayout() {
-  const initAuth = useStore(s => s.initAuth);
-
-  useEffect(() => { 
-    initAuth(); 
-  }, []);
-
 export default function OnboardingLayout() {
   return (
     <OnboardingProvider>
